f9f3ef85e9cdd16f284aacfb184a2b137ce72f30
Shwetank Saptarshi <Shwetank.Saptarshi@mea.ey.net>
2018-06-26 16:12:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs= require('fs');
+
+var app = express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+return text.toUpperCase();
+});
+
+// Middelware
+// -----------------------------------------------------------------------------
+
+app.use((req,res,next)=>{
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('Log.txt',log+'\n',(err)=>{
+        if(err)
+        {
+            console.log('Uhnable to write to console log file');
+        }
+
+    });
+    next();
+});
+
+app.use((req,res,next)=>{
+res.render('maintenance.hbs',{
+    pageBody : 'Maintenance page rendered'
+})
+});
+
+app.use(express.static(__dirname+'/public'));
+// -----------------------------------------------------------------------------
+
+
+
+app.get('/',(req,res)=>{
+    //res.send('Helo Express');
+    //res.send('<b>Hello Express !</b>');
+    // res.send({
+    //     name :'Andrew',
+    //     intrests: ['A','B','C','D']
+    // });
+    res.render('home.hbs',{
+        welcomeMessage: 'Welcome Page',
+        pageBody : 'Rendered Home page body',
+        currentYear :  new Date().getFullYear()
+    });
+});
+
+app.get('/about',(req,res)=>{
+    //res.send('About Page');
+    res.render('about.hbs',{
+        pageBody : 'Rendered About page title',
+        currentYear :  new Date().getFullYear()
+    });    
+});
+
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorCode : 404,
+        errorMessage : 'Page not found' 
+    });
+});
+app.listen(3000,()=>{
+    console.log('Server started on port 3000');
+});
